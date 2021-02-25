80f6adf6070d9df2a31b5073462b649745d4be11
pihlnikl <niklas_pihl@hotmail.com>
2021-02-25 09:16:12 +0200

    Flyttade proj1 till egen folder + ny proj2 folder

---
+++ Proj2/index.php
@@ -0,0 +1,9 @@
+<?php include "init.php" ?>
+<?php include "head.php" ?>
+
+<article>
+    <h1>Hämta data</h1>
+    <?php include "fetch.php" ?>
+</article>
+
+<?php include "footer.php" ?>
---
+++ Proj2/init.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+// Remove whitespaces, remove extra slashes, and convert to safe html characters
+// USE FOR ALL USER INPUT
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+// Sets up connection to database - use $conn = create_conn(); to open connection and $conn->close();
+function create_conn()
+{
+    // byt error reporting läge
+    mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
+    //Databaskonfiguration
+    $servername = "localhost";
+    $username = "pihlnikl";
+    $password = "asfzjLdp9G";
+    $dbname = "backend_proj2";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        return $conn;
+    }
+}
